e6fa4bc004dd905b08457208d95607bd65264ee3
Exodus ✝️ 🇵🇸 <108364522+earthtoexodus@users.noreply.github.com>
2025-01-21 13:10:28 -0500

    quick redo

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.getElementById('contact-form').addEventListener('submit', async (e) => {
-    e.preventDefault(); // Prevent form submission
+    e.preventDefault();
   
     const webhookUrl = 'https://discord.com/api/webhooks/1331313016901206017/kIYLkSaItQVZcur57_wbC2dJQZUAnp9SVf7scR1dbAWr-PMUymjHCesAc5OTey-6UIgs';
     const username = document.getElementById('discord-user').value;
     const message = document.getElementById('msg').value;
   
-    // Construct the payload
     const payload = {
       content: `# __NEW MESSAGE__!
 **USER:** ${username}
